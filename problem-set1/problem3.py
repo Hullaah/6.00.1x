66a15cb07afff8bc453068919ce164bec3770313
hullaah <umaradelo1.247@gmail.com>
2023-06-09 15:46:05 +0100

    modified problem 3 because it wasn't meeting some edge cases

--- problem-set1/problem3.py
+++ problem-set1/problem3.py
@@ -1,29 +1,15 @@
-s = 'luwscraoflpr'
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-arr_longest, longest = [], []
-j, len_s, prev = 0, len(s), 0
-while j < len_s:
-    for i in range(26):
-        if alphabet[i] == s[j]:
-            break
-    if j == 0:
-        longest.append(s[j])
-    else:
-        if prev > i:
-            arr_longest.append(longest)
-            longest = []
-            longest.append(s[j])
-        elif prev < i or prev == i:
-            longest.append(s[j])
-    prev = i
-    j += 1
-if not arr_longest:
-    arr_longest.append("".join(longest)) 
-long = [-1, 0]
-for i in range(len(arr_longest)):
-    len_elem = len(arr_longest[i])
-    if len_elem > long[0]:
-        long[0] = len_elem
-        long[1] = i
-
-print("Longest substring in alphabetical order is:", "".join(arr_longest[long[1]]))
+s = input("Enter string: ")
+string = ""
+prev = 26
+for char in s:
+    current = ord(char) - 97
+    string += char if current >= prev else " " + char
+    prev = current
+string = string.split(" ")
+max = -1
+longest = ""
+for val in string:
+    if len(val) > max:
+        max = len(val)
+        longest = val
+print("Longest substring in alphabetical order is:", longest)
